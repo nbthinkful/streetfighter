9b78f865d58bad6593acdbc170fbb3eb7da39a37
neville <combat>
2014-12-30 14:46:07 +0100

    Added the street fighter task deployed with css animations

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function playHadouken(){
+	var sound = document.getElementById("hadouken-sound");
+	sound.volume = 0.5;
+	sound.currentTime = 0;
+	sound.play();
+};
+
+function showRyuPosture(className) {
+	var postures = $('.ryu-posture');
+	$.each(postures, function (index, value) {
+		var ryuPosture = $(value);
+		
+		if (ryuPosture.hasClass(className)) {
+			ryuPosture.show();
+		} else {
+			ryuPosture.hide();
+		};
+	});	
+};
+
+$(document)
+	.ready(function() {	
+		$('.ryu')
+			.mouseenter(function(){		
+				showRyuPosture('ryu-ready');
+			})
+			.mouseleave(function(){
+				showRyuPosture('ryu-still');
+			})
+			.mousedown(function(){
+				playHadouken();
+				showRyuPosture('ryu-throwing');
+				var hadouken = $('.hadouken').addClass('animate');
+				var hadoukenClone = hadouken.clone(true);
+				hadouken.replaceWith(hadoukenClone);
+			})
+			.mouseup(function(){
+				showRyuPosture('ryu-ready');
+			});
+	})
+	.keydown(function(e){
+		if (e.keyCode == 88){
+			showRyuPosture('ryu-cool');
+		}
+	})
+	.keyup(function(e){
+		if (e.keyCode == 88){
+			showRyuPosture('ryu-ready');
+		}
+	})
